26aeb4a106973c26b6163c36a6116a9379b92396
Morad <usama.morad@gmail.com>
2020-10-21 21:20:30 +0200

    Configuration: Project structure setup with maven and JUnit5 dependency

---
+++ src/main/java/com/cgm/at/knowledgebase/Main.java
@@ -0,0 +1,16 @@
+package com.cgm.at.knowledgebase;
+
+/**
+ * Main class, the entry point of the Application.
+ * The Application provides two options, first is to add a question to the knowledge base and second to ask a question of it.
+ * If the question isn't present in the knowledge base, then the answer will be "".
+ *
+ * @author Usama Morad
+ * @version 1.0
+ * @date 21.10.2020
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
