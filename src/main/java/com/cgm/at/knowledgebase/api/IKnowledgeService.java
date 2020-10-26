99bb5547f13731f4421a42bb56e5250f77025bda
usamamorad <usama.morad@gmail.com>
2020-10-26 16:40:06 +0100

    Architecture: extended exception handling for Knowledgebase, extended documentation. Extended interfaces for serialized input type.

--- src/main/java/com/cgm/at/knowledgebase/api/IKnowledgeService.java
+++ src/main/java/com/cgm/at/knowledgebase/api/IKnowledgeService.java
@@ -1,19 +1,21 @@
 package com.cgm.at.knowledgebase.api;
 
+import com.cgm.at.knowledgebase.exceptions.KnowledgeBaseException;
 import java.util.List;
 
 /**
- * IKnowledgeService defines which functionality a KnowledgeBase should have.
- * Either to ask a question or to add knowledge to the Knowledge base.
- * This KnowledgeBase is implemented using generics to allow future implementations using more complex types of question and answer.
+ * IKnowledgeService defines which functionality a knowledge base should have.
+ * Ae knowledge base is a Collection of questions and related answers, where a question can have unlimited answers, but at least one.
+ * It allows for two operations either to ask a question or to add knowledge to the Knowledge base.
+ * This knowledge base is implemented using generics to allow future implementations using more complex types of question and answer.
  *
  * @author Usama Morad
  * @version 1.0
  * @param <Q> Generic type of Question
  * @param <A> Generic type of Answer
- * @see IKnowledgeValidationService
+ * @param <S> Generic type of serialized Knowledge.
  */
-public interface IKnowledgeService<Q, A> {
+public interface IKnowledgeService<Q, A, S> {
     /**
      * Returns a list of all Answers of the asked question.
      * If the question asked is not in the KnowledgeBase,
@@ -22,14 +24,33 @@ public interface IKnowledgeService<Q, A> {
      *                 needs to be validated using {@link IKnowledgeValidationService}.
      * @return a List of answers of type A to the question asked,
      *         otherwise a default answer of type A will be returned as the only item in the list.
+     * @see IKnowledgeValidationService
+     * @throws IllegalArgumentException thrown if the IKnowledgeValidationService invalidates the question given.
+     * @throws KnowledgeBaseException thrown if the question cannot be asked due to collection implementation errors.
      */
-    List<A> askQuestion(Q question) throws IllegalArgumentException;
+    List<A> askQuestion(Q question) throws IllegalArgumentException, KnowledgeBaseException;
 
     /**
      * Adds the given question of type Q and adds the given answers to it of type A to the knowledge base.
      * question and answers needs to be validated using {@link IKnowledgeValidationService}.
      * @param question of type Q, example: using type String: "what is james favorite color?"
      * @param answers a List of Type A, the list must not be empty!
+     * @see IKnowledgeValidationService
+     * @return true if the knowledge has been added to the knowledge base successfully, otherwise false.
+     * @throws IllegalArgumentException thrown if the {@link IKnowledgeValidationService} invalidates the {@param question} or {@param answers} given.
+     * @throws KnowledgeBaseException thrown if the question cannot be asked due to collection implementation errors.
      */
-    void addKnowledge(Q question, List<A> answers) throws IllegalArgumentException;
+    boolean addKnowledge(Q question, List<A> answers) throws IllegalArgumentException, KnowledgeBaseException;
+
+    /**
+     * Adds the given serialized knowledge to the knowledge base by first extracting the question and answers from it,
+     * and then adding it to the knowledge base. The {@link IKnowledgeValidationService} need to be used to validate input.
+     * @param serializedKnowledge Contains both the question, and the related answers inside of it as one serialized information.
+     * @throws IllegalArgumentException thrown if the serialized knowledge does not comply to the rules defined in {@link IKnowledgeValidationService}.
+     * @see IKnowledgeValidationService
+     * @return true if the knowledge has been added to the knowledge base successfully, otherwise false.
+     * @throws IllegalArgumentException thrown if the {@link IKnowledgeValidationService} invalidates the {@param serializedKnowledge} given.
+     * @throws KnowledgeBaseException thrown if the question cannot be asked due to collection or deserialization implementation errors.
+     */
+    boolean addKnowledge(S serializedKnowledge) throws IllegalArgumentException, KnowledgeBaseException;
 }
--- src/main/java/com/cgm/at/knowledgebase/common/KnowledgeBase.java
+++ src/main/java/com/cgm/at/knowledgebase/common/KnowledgeBase.java
@@ -2,6 +2,7 @@ package com.cgm.at.knowledgebase.common;
 
 import com.cgm.at.knowledgebase.api.IKnowledgeService;
 import com.cgm.at.knowledgebase.api.IKnowledgeValidationService;
+import com.cgm.at.knowledgebase.exceptions.KnowledgeBaseException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -11,19 +12,20 @@ import java.util.Map;
  * uses {@link IKnowledgeValidationService} to validate input.
  * @param <Q> generic type of the Question stored in the KnowledgeBase.
  * @param <A> generic type of the Answer stored in the KnowledgeBase.
+ * @param <S> generic type of the serialized knowledge input.
  * @see IKnowledgeService
  * @see IKnowledgeValidationService
  *
  * @author Usama Morad
  * @version 1.0
  */
-public abstract class KnowledgeBase<Q, A> implements IKnowledgeService<Q, A> {
+public abstract class KnowledgeBase<Q, A, S> implements IKnowledgeService<Q, A, S> {
 
     // a map containing all knowledge of the knowledge base.
     private Map<Q, List<A>> knowledge;
 
     // a validation service to validate inputs.
-    private IKnowledgeValidationService<A, Q> validationService;
+    private IKnowledgeValidationService<Q, A, S> validationService;
 
     // the default value if a question is not in the knowledge base.
     private A defaultValue;
@@ -35,7 +37,7 @@ public abstract class KnowledgeBase<Q, A> implements IKnowledgeService<Q, A> {
      * @param validationService a validation service to validate inputs.
      * @param defaultValue a default answer of generic type A used to be the answer for the unknown.
      */
-    public KnowledgeBase(Map<Q, List<A>> knowledge, IKnowledgeValidationService<A, Q> validationService, A defaultValue) {
+    public KnowledgeBase(Map<Q, List<A>> knowledge, IKnowledgeValidationService<Q, A, S> validationService, A defaultValue) {
         this.knowledge = knowledge;
         this.validationService = validationService;
         this.defaultValue = defaultValue;
@@ -45,18 +47,44 @@ public abstract class KnowledgeBase<Q, A> implements IKnowledgeService<Q, A> {
      * {@inheritDoc}
      */
     @Override
-    public List<A> askQuestion(Q question) throws IllegalArgumentException {
+    public List<A> askQuestion(Q question) throws IllegalArgumentException, KnowledgeBaseException {
         this.validationService.validateQuestion(question);
-        return knowledge.getOrDefault(question, Collections.singletonList(defaultValue));
+        try {
+            return knowledge.getOrDefault(question, Collections.singletonList(defaultValue));
+        } catch (NullPointerException | ClassCastException e) {
+            throw new KnowledgeBaseException("Error while asking a Question to the knowledge base: " + e.getMessage(), e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void addKnowledge(Q question, List<A> answers) throws IllegalArgumentException {
+    public boolean addKnowledge(Q question, List<A> answers) throws IllegalArgumentException, KnowledgeBaseException {
         this.validationService.validateQuestion(question);
         this.validationService.validateAnswers(answers);
-        knowledge.put(question, answers);
+        try {
+            this.knowledge.put(question, answers);
+        } catch (NullPointerException | ClassCastException e) {
+            throw new KnowledgeBaseException("Error while adding a Question to the knowledge base: " + e.getMessage(), e);
+        }
+        return true;
+    }
+
+    /**
+     * Gets the knowledge map
+     * @return a map contains all knowledge of the knowledge base.
+     */
+    public Map<Q, List<A>> getAllKnowledge(){
+        return this.knowledge;
+    }
+
+    /**
+     * Gets the validation service
+     * @return A {@link IKnowledgeValidationService}
+     * @see IKnowledgeValidationService
+     */
+    public IKnowledgeValidationService getValidationService(){
+        return this.validationService;
     }
 }
