ae83bd780e3ca09da23b9327e5ef40b348b9e205
usamamorad <usama.morad@gmail.com>
2020-10-26 16:42:24 +0100

    Architecture: Extended validation interface for serialized input type validation, fixed generic type order, extended documentation.

--- src/main/java/com/cgm/at/knowledgebase/api/IKnowledgeValidationService.java
+++ src/main/java/com/cgm/at/knowledgebase/api/IKnowledgeValidationService.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 /**
  * IKnowledgeValidationService is responsible for validating the knowledge before it gets saved in the knowledge base.
- * this interface makes use of generics, so if the question is an audio wave, the implementer need to make sure that the sound wave captured is valid.
+ * this interface makes use of generics, so if the question is an audio wave, the implementer need to make sure, that the sound wave captured is valid.
  * another simpler example is when the question is in written language as text, then the implementer must take considerations accordingly.
  *
  * @author Usama Morad
  * @version 1.0
- * @param <Q> Generic type of Question
- * @param <A> Generic type of Answer
+ * @param <Q> Generic type of Question.
+ * @param <A> Generic type of Answer.
+ * @param <S> Generic type of serialized knowledge.
  */
-public interface IKnowledgeValidationService<A, Q> {
+public interface IKnowledgeValidationService<Q, A, S> {
     /**
      * validates a question given, a question is a sentence worded or expressed so as to elicit information.
      * the following criteria defines a valid question:
@@ -42,9 +43,18 @@ public interface IKnowledgeValidationService<A, Q> {
      * examples for invalid answers:
      * > empty sound wave, empty string or null string, string length greater than predefined maximum length
      * @param answers an answer is a thing that is said, written, or done as a reaction to a question, statement, or situation.
-     * @throws IllegalArgumentException thrown if the following criteria does not meet:
+     * @throws IllegalArgumentException thrown if at least the following criteria does not meet:
      *         1) non emptiness, information must be given.
      *         2) length of answer does not exceed the predefined maximum length
      */
     void validateAnswers(List<A> answers) throws IllegalArgumentException;
+
+    /**
+     * validates the serialized knowledge given of generic type S.
+     * This method does not validate the question and answers itself, only the format of the serialization input.
+     * @param serializedKnowledge Contains both the question, and the related answers inside of it as one serialized information.
+     * @throws IllegalArgumentException thrown if the knowledge cannot be extracted from the serialization given due to noncompliance to a format defined in the specific type-implementation of this interface.
+     * dependant of the type of Question and Answer, the implementation of the specific validation class, need to define rules, so this serialization is valid.
+     */
+    void validateSerializedKnowledgeFormat(S serializedKnowledge) throws IllegalArgumentException;
 }
